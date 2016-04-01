a21da3c8af4f5a0f3f7a3b94b96b38870d3b032f
a.uralov <a.uralov@wis.eu.com>
2016-04-01 10:42:27 +0300

    Create TaskDecline and Task Attachment models

--- src/task_manage/models.py
+++ src/task_manage/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
     STATUS_DECLINED = 0
     STATUS_DRAFT = 1
     STATUS_PENDING = 2
-    STATUS_IN_WORK = 3
+    STATUS_WORKING = 3
     STATUS_HALF_DONE = 4
     STATUS_ALMOST_DONE = 5
     STATUS_COMPLETE = 6
@@ -15,7 +15,7 @@ class Task(models.Model):
         (STATUS_DECLINED, 'Declined'),
         (STATUS_DRAFT, 'Draft'),
         (STATUS_PENDING, 'Pending Acceptance'),
-        (STATUS_IN_WORK, 'Working On It'),
+        (STATUS_WORKING, 'Working On It'),
         (STATUS_HALF_DONE, 'Half Way Done'),
         (STATUS_ALMOST_DONE, 'Almost Done'),
         (STATUS_COMPLETE, 'Completed'),
@@ -30,7 +30,7 @@ class Task(models.Model):
         (CRITICALITY_HIGH, 'High criticality'),
     )
 
-    title = models.CharField('Task title')
+    title = models.CharField('Task title', max_length=4096)
     description = models.TextField('Task description')
     assigned_to = models.ManyToManyField(User, blank=True,
                                          verbose_name='Task assigned to')
@@ -40,3 +40,19 @@ class Task(models.Model):
     date_due = models.DateField('Task due date', blank=True, null=True)
     time_create = models.DateTimeField('Time of create', auto_now_add=True)
     time_update = models.DateTimeField('Time of update', auto_now=True)
+
+
+class TaskDecline(models.Model):
+    """ Declined tasks model """
+    task = models.ForeignKey(Task, related_name='declines',
+                             verbose_name='Declined task')
+    reason = models.TextField('Reason for decline')
+    time_decline = models.DateTimeField('Time of decline', auto_now_add=True)
+
+
+class TaskAttachment:
+    """ Task attachments model """
+    task = models.ForeignKey(Task, related_name='attachments',
+                             verbose_name='Attachment task')
+    attachment = models.FileField('Attachment',
+                                  upload_to='task_attachment/%Y/%m/%d/')
