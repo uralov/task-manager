0b93c17f845bc9e734be223603e11d1f9dc46fe6
a.uralov <a.uralov@wis.eu.com>
2016-04-04 14:30:58 +0300

    Detail view creation

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -6,4 +7,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^task_management/', include('task_management.urls',
                                       namespace='task_management')),
+    url(
+        r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}
+    ),
 ]
